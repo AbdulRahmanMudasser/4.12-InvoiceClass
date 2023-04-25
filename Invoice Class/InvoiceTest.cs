2f103c8d595690ffc5a82bf6b223ea029d8783b0
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-25 13:49:28 +0500

    done

---
+++ Invoice Class/InvoiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice_Class
+{
+    internal class InvoiceTest
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice("342801", "Gaming Mouse", 3, 450.12m);
+
+            Console.WriteLine("Invoice \n\nPart Number: {0} \nPart Description: {1} \nQuantity: {2} \nPrice Per Item: {3:C2} \nTotal: {4:C2}", invoice.PartNumber, invoice.PartDescription, invoice.Quantity, invoice.Price, invoice.GetInvoiceAmount());
+        }
+    }
+}
