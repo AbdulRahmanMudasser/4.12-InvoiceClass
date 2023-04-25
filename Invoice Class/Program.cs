7cce79f904eb13aa14e10221378906a1c05f58ff
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-25 13:19:22 +0500

    variables

--- Invoice Class/Program.cs
+++ Invoice Class/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Invoice
+{
+    // declaring variables
+    private string partNumber;
+    private string partDescription;
+    private int quantity;
+    private decimal price;
+
+    
+}
