da5dec1efad55081885f3eeeee6b9aa88ce71fb6
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-25 13:24:14 +0500

    properties

--- Invoice Class/Program.cs
+++ Invoice Class/Program.cs
@@ -6,5 +6,40 @@
     private int quantity;
     private decimal price;
 
-    
+    // properties
+    public string PartNumber
+    {
+        get { return partNumber; }
+        set { partNumber = value; }
+    }
+
+    public string PartDescription
+    {
+        get { return partDescription; }
+        set { partDescription = value; }
+    }
+
+    public int Quantity
+    {
+        get { return quantity; }
+        set
+        {
+            if (quantity >= 0)
+            {
+                quantity = value;
+            }
+        }
+    }
+
+    public decimal Price
+    {
+        get { return price; }
+        set
+        {
+            if (price >= 0)
+            {
+                price = value;
+            }
+        }
+    }
 }
